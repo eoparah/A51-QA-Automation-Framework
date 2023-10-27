546205eb35acdb83fc0593d6af20810e2af35e8a
eoparah <esther.oparah@testpro.io>
2023-10-26 21:29:10 -0500

    Homework17
    Almost passed :) I could not figure out how to find the correct locator for a popup notification.

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,25 +1,23 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class LoginTests extends BaseTest {
+
     @Test
     public void loginEmptyEmailPassword() {
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+    launchBrowser();
+    navigateToLoginPage();
+    provideEmail("demo@class.com");
+    providePassword("te$tStudent");
+    clickSubmit();
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+    WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+    Assert.assertEquals(driver.getCurrentUrl(), url);
+    driver.quit();
     }
+
+
 }
