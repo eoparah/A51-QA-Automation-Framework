ae8ff3e9fa373a14a2110c22c33dd9ec32a10a79
eoparah <esther.oparah@testpro.io>
2023-11-13 01:30:40 -0600

    Homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,7 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -30,11 +35,11 @@ public class BaseTest {
     @BeforeMethod
     @Parameters({"BaseURL"})
     public void launchBrowser(String BaseURL){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
-        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
 
-        driver = new ChromeDriver(options);
+        //options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        driver = pickBrowser(System.getProperty("browser"));
+
+        //driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         actions = new Actions (driver);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -42,6 +47,40 @@ public class BaseTest {
         url = BaseURL;
 
     }
+        public static WebDriver pickBrowser(String browser){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            String gridURL = "http://10.0.0.206.4444";
+
+            switch (browser){
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    return driver = new FirefoxDriver();
+                case "MicrosoftEdge":
+                    WebDriverManager.chromedriver().setup();
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    edgeOptions.addArguments("--remote-allow-origins=*");
+                    return driver = new EdgeDriver(edgeOptions);
+
+                    //Selenium Grid
+                case "grid-edge": //gradle clean test -Dbrowser=grid-edge
+                    caps.setCapability("browser", "MicrosoftEdge");
+                    return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+                case "grid-firefox": //gradle clean test -Dbrowser=grid-firefox
+                    caps.setCapability("browserName", "firefox");
+                    return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+                case "grid-chrome": //gradle clean test -Dbrowser=grid-chrome
+                    caps.setCapability("browserName", "chrome");
+                    return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+                    default:
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
+                    return driver = new ChromeDriver(options);
+            }
+        }
 
     @AfterMethod
     public void closeBrowser(){
