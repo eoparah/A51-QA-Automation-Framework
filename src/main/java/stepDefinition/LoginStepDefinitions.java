b5c99628d4ff4f0c79a434f461c9d61074addc17
eoparah <esther.oparah@testpro.io>
2023-11-28 20:51:02 -0600

    Homework27
    I could not figure out how to successfully import the WebDriverManager. Please advise, thank you.

---
+++ src/main/java/stepDefinition/LoginStepDefinitions.java
@@ -0,0 +1,62 @@
+package stepDefinition;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class LoginStepDefinitions {
+
+    WebDriver driver;
+
+    WebDriverWait wait;
+
+    @Before
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @After
+    public void closeBrowser(){
+        driver.quit();
+    }
+    @Given("I open Login Page")
+    public void iOpenLoginPage() {
+        driver.get("https://qa.koel.app/");
+    }
+    @When("I enter email {string}")
+    public void iEnterEmail(String arg0) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']"))).sendKeys(email);
+    }
+    @And("I enter password {string}")
+    public void iEnterPassword(String arg0) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']"))).sendKeys(password);
+        }
+
+    @And("I submit")
+    public void iSubmit(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='submit']"))).click();
+    }
+
+    @Then("I should get logged in")
+    public void iShouldGetLoggedIn() {
+        Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+    }
+}
